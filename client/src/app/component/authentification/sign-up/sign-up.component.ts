cac9e9d0be512e5d6a113872006e77c9ae2d5bc2
cyrilpluche <cyril.pluche@etu.umontpellier.fr>
2018-06-07 14:22:11 +0200

    Sign up verifications

--- client/src/app/component/authentification/sign-up/sign-up.component.ts
+++ client/src/app/component/authentification/sign-up/sign-up.component.ts
@@ -9,15 +9,27 @@ export class SignUpComponent implements OnInit {
 
   /* ----- Data ----- */
   step1: boolean = true;
-  organisationEnabled: boolean = true;
   branchEnabled: boolean = false;
   departmentEnabled: boolean = false;
   subDepartmentEnabled: boolean = false;
+
+  firstName: string = "";
+  lastName: string = "";
+  email: string = "";
+  emailConfirmation: string = "";
+  password: string = "";
+  passwordConfirmation: string = "";
 
   organisationChoosen: string = "";
   branchChoosen: string = "";
   departmentChoosen: string = "";
   subDepartmentChoosen: string = "";
+
+  errorMessage: string = "";
+
+  //Norme RFC2822 email validation
+  emailReg = new RegExp( /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+
 
   constructor() { }
 
@@ -25,11 +37,51 @@ export class SignUpComponent implements OnInit {
   }
 
   onSubmit() {
-
+    console.log("I'm here");
+    if (this.organisationChoosen == "") {
+      this.errorMessage = "Organisation name required.";
+    }
+    else {
+      console.log("Sign up ok.");
+      this.errorMessage = "";
+    }
   }
 
   changeStep() {
-    this.step1 = !this.step1;
+    this.step1 = true;
+    this.errorMessage = "";
+  }
+
+  firstStepVerification() {
+    if (this.firstName == null || this.firstName == "") {
+      this.errorMessage = "First name is required.";
+    }
+    else if (this.lastName == null || this.lastName == "") {
+      this.errorMessage = "Last name is required.";
+    }
+    else if (this.email == null || this.email == "") {
+      this.errorMessage = "Email is required.";
+    }
+    else if (!this.emailReg.test(this.email)) {
+      this.errorMessage = "Please enter a valid email."
+    }
+    else if (this.emailConfirmation != this.email) {
+      console.log(this.emailConfirmation);
+      console.log(this.email);
+      this.errorMessage = "Email confirmation is wrong.";
+    }
+    else if (this.password == null || this.password == "") {
+      this.errorMessage = "Password is required";
+    }
+    else if (this.passwordConfirmation != this.password) {
+      this.errorMessage = "Password confirmation is wrong.";
+    }
+    else {
+      //Connexion code
+      this.step1 = false;
+      console.log("Next");
+      this.errorMessage = "";
+    }
   }
 
   // Select fields authorizations
