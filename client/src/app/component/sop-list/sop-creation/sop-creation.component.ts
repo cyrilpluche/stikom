b1fce41dd5efbfed6e7134298ea132bce11e6c63
cyrilpluche <cyril.pluche@etu.umontpellier.fr>
2018-06-21 08:51:05 +0200

    SOP Creation - pagination

--- client/src/app/component/sop-list/sop-creation/sop-creation.component.ts
+++ client/src/app/component/sop-list/sop-creation/sop-creation.component.ts
@@ -9,6 +9,8 @@ import {current} from "codelyzer/util/syntaxKind";
 export class SopCreationComponent implements OnInit {
 
   /* ----- Data ----- */
+  errorMessage: string = "";
+
   rules: string[] = [];
   units: string[] = [];
   warnings: string[] = [];
@@ -36,45 +38,85 @@ export class SopCreationComponent implements OnInit {
     this.captionSelected = this.captions[0];
     this.elementsArray = this.rules;
     this.stepSelected = 0;
+    //document.querySelector(this.captions[2]).className += ".active";
   }
 
-  changeCaption(stepNumber) {
-    this.stepSelected = stepNumber;
-    this.captionSelected = this.captions[stepNumber];
-    if (stepNumber==0) {
+
+  //Enable to change page with the arrows
+  arrowChangeCaption(next, $event) {
+
+    if (next && this.stepSelected < 6) {
+      this.changeCaption(this.captions[this.stepSelected+1], $event);
+
+    }
+    else if (!next && this.stepSelected > 0) {
+      this.changeCaption(this.captions[this.stepSelected-1], $event);
+    }
+
+  }
+
+  // This function change the page selected and set the right array to the view list
+  changeCaption(stepNumber, $event) {
+    console.log($event);
+    this.errorMessage = "";
+    this.stepSelected = this.captions.indexOf(stepNumber);
+    this.captionSelected = this.captions[this.stepSelected];
+    if (this.stepSelected==0) {
       this.elementsArray = this.rules;
     }
-    else if (stepNumber==1) {
+    else if (this.stepSelected==1) {
       this.elementsArray = this.units;
     }
-    else if (stepNumber==2) {
+    else if (this.stepSelected==2) {
       this.elementsArray = this.warnings;
     }
-    else if (stepNumber==3) {
+    else if (this.stepSelected==3) {
       this.elementsArray = this.staffQualifications;
     }
-    else if (stepNumber==4) {
+    else if (this.stepSelected==4) {
       this.elementsArray = this.supportingTools;
     }
-    else if (stepNumber==5) {
+    else if (this.stepSelected==5) {
       this.elementsArray = this.typesFormsReports;
     }
+    this.numberNavigation($event, stepNumber);
   }
 
   addNewRule () {
-    if (this.newRule != "" && !this.rules.includes(this.newRule)) {
+    if (this.newRule == "") {
+      this.errorMessage = "Rule indication field is required.";
+    }
+    else if (this.rules.includes(this.newRule)) {
+      this.errorMessage = "This rule indication already exists.";
+    }
+    else {
       this.rules.push(this.newRule);
+      this.errorMessage = "";
     }
   }
 
   addNewUnit () {
-    if (this.newUnit != "" && !this.units.includes(this.newUnit)) {
+    if (this.newUnit == "") {
+      this.errorMessage = "Unit name is required.";
+    }
+    else if (this.units.includes(this.newUnit)) {
+      this.errorMessage = "This unit name already exists.";
+    }
+    else {
       this.units.push(this.newUnit);
+      this.errorMessage = "";
     }
   }
 
   addNewWarning () {
-    if (this.newWarning != "" && !this.warnings.includes(this.newWarning)) {
+    if (this.newWarning == "") {
+      this.errorMessage = "Warning indication field is required.";
+    }
+    else if (this.warnings.includes(this.newWarning)) {
+      this.errorMessage = "This warning indication already exists.";
+    }
+    else {
+      this.errorMessage = "";
       this.warnings.push(this.newWarning);
     }
   }
@@ -113,18 +155,38 @@ export class SopCreationComponent implements OnInit {
     }
   }
 
-  // Active management of the number navigation
-  numberNavigation($event){
+  // Active management of the number navigation background
+  numberNavigation($event, stepName){
 
     let clickedElement = $event.target || $event.srcElement;
+    console.log(clickedElement.className);
+    console.log(clickedElement.className == "page-link no-hover fa fa-arrow-left" && this.stepSelected > -1);
+    if (clickedElement.className == "page-link" || (clickedElement.className == "page-link no-hover fa fa-arrow-right" && this.stepSelected<7) || (clickedElement.className == "page-link no-hover fa fa-arrow-left" && this.stepSelected > -1)){
 
-    if(clickedElement) {
       let currentNumberActive = clickedElement.parentElement.parentElement.querySelector(".active");
+
       if (currentNumberActive) {
         currentNumberActive.classList.remove("active");
       }
-      clickedElement.parentElement.className += " active";
+
+
+      if (clickedElement.className == "page-link") {
+        clickedElement.parentElement.className += " active";
+      }
+      if (clickedElement.className == "page-link no-hover fa fa-arrow-left" && this.stepSelected > -1) {
+        let id = "#"+stepName;
+        console.log(id)
+        document.querySelector(id).className += " active";
+      }
+      if (clickedElement.className == "page-link no-hover fa fa-arrow-right" && this.stepSelected < 7) {
+
+        let id = "#"+stepName;
+        console.log(id)
+        document.querySelector(id).className += " active";
+      }
     }
+
+
 
   }
 
