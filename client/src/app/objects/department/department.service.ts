880321295f877200bade26c231e7758998243faf
James <jamesterrien95@hotmail.fr>
2018-06-21 19:10:50 +0800

    script update

--- client/src/app/objects/department/department.service.ts
+++ client/src/app/objects/department/department.service.ts
@@ -1,9 +1,30 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DepartmentService {
 
-  constructor() { }
+  domain = environment.SERVEUR_URL;
+  httpOptions;
+  constructor(private http:HttpClient) {
+
+  }
+
+  generateHeaders()
+  {
+    this.httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': localStorage.getItem("Token")
+      })
+    };
+  }
+
+  selectAllFromBranch(branch:string){
+    this.generateHeaders();
+    return this.http.get(this.domain + '/api/department/all?branch='+branch,this.httpOptions);
+  }
 }
--- client/src/app/objects/sub_department/sub-department.service.ts
+++ client/src/app/objects/sub_department/sub-department.service.ts
@@ -1,9 +1,30 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SubDepartmentService {
 
-  constructor() { }
+  domain = environment.SERVEUR_URL;
+  httpOptions;
+  constructor(private http:HttpClient) {
+
+  }
+
+  generateHeaders()
+  {
+    this.httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': localStorage.getItem("Token")
+      })
+    };
+  }
+
+  selectAllFromDepartment(department:string){
+    this.generateHeaders();
+    return this.http.get(this.domain + '/api/sub_department/all?department='+department,this.httpOptions);
+  }
 }
