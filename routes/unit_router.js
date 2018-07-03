2b279b0d7c0fd2f1da88d54b67a895186a77f4ca
michelexe <enzo.fabre@orange.fr>
2018-07-03 12:06:19 +0200

    selectionner toutes les unit à partir d'un sop_id creeer un unit insérer dans la table unit lier un projet et un job
    Signed-off-by: michelexe <enzo.fabre@orange.fr>

--- routes/unit_router.js
+++ routes/unit_router.js
@@ -13,7 +13,22 @@ router.post('/create', policy.checkParameters(['unit_name']),
                 res.json({data: data});
             }
         }).catch(next)
-    });
+    }
+);
+
+router.post('/execute_activity',
+    policy.checkParameters(['unit_id', 'activity_id']),
+    function (req, res, next) {
+        modelUnit.insertExecute(req.body.unit_id, req.body.activity_id)
+            .then(function (data) {
+                if (!data) {
+                    throw ERRORTYPE.INTERNAL_ERROR
+                } else {
+                    res.json({data: data});
+                }
+            }).catch(next)
+    }
+);
 
 router.get('/all_from_sop/:sop', function (req, res, next) {
     modelUnit.selectAllBySopId(req.params.sop).then(function (data) {
