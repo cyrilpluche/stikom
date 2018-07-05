efb3e655c6bb7f61ab057b710c5460b0ab24953c
James <jamesterrien95@hotmail.fr>
2018-07-05 15:00:06 +0800

    pdf sop 2 pages 100%

---
+++ client/src/app/documents/pdf-gantt/pdf-gantt.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pdf-gantt',
+  templateUrl: './pdf-gantt.component.html',
+  styleUrls: ['./pdf-gantt.component.scss']
+})
+export class PdfGanttComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
