fd20f8e734f7eeff5392d6d70f36d6c239ac7630
cyrilpluche <cyril.pluche@etu.umontpellier.fr>
2018-07-11 11:42:51 +0200

    Back up gantt-creation

--- client/src/app/component/project-list/volume-progress/volume-progress.component.ts
+++ client/src/app/component/project-list/volume-progress/volume-progress.component.ts
@@ -5,6 +5,8 @@ import {Member} from "../../../objects/member/member";
 import {Activity} from "../../../objects/activity/activity";
 import {MemberService} from "../../../objects/member/member.service";
 import {ActivityService} from "../../../objects/activity/activity.service";
+import {Project} from "../../../objects/project/project";
+import {ProjectService} from "../../../objects/project/project.service";
 
 @Component({
   selector: 'app-volume-progress',
@@ -15,7 +17,7 @@ export class VolumeProgressComponent implements OnInit {
 
   /* ----- Data ----- */
   errorMessage: string = "";
-  project_id:string;
+  project: Project;
   m_a_p: MemberActivityProject[] = [];
   workers: Member[] = [];
   activities: Activity[] = [];
@@ -24,25 +26,39 @@ export class VolumeProgressComponent implements OnInit {
 
   ready:boolean = false;
 
+  months_label = [];
+  month_index:number = 0;
 
   constructor(private _memberActivityProjectService: MemberActivityProjectService,
               private _memberService: MemberService,
-              private _activityService: ActivityService) { }
+              private _activityService: ActivityService,
+              private _projectService: ProjectService) { }
 
   async ngOnInit() {
-    this.project_id = localStorage.getItem('Project_id');
     await this.loadData();
     await this.sortElements();
+    this.calendarRange();
   }
 
+  /* Load the project & all m_a_p data with the activity & member linked */
   async loadData() {
 
     this.ready = false;
 
+    //We select the project
+    try {
+      let project_id = localStorage.getItem('Project_id');
+      let project = await this._projectService.select(project_id).toPromise();
+      this.project = project['data'];
+    }
+    catch (error) {
+      this.errorMessage = error.error.message;
+    }
+
     //We select all MemberActivityProject linked to the project
     this.m_a_p = [];
     try {
-      let m_a_p = await this._memberActivityProjectService.selectAllFromProject(this.project_id).toPromise();
+      let m_a_p = await this._memberActivityProjectService.selectAllFromProject(this.project.project_id).toPromise();
       this.m_a_p = m_a_p['data'];
     }
     catch (error) {
@@ -65,12 +81,11 @@ export class VolumeProgressComponent implements OnInit {
       this.errorMessage = error.error.message;
     }
 
-    console.log('Worker array : ', typeof this.workers);
-    console.log('M_A_P array : ', typeof this.m_a_p);
     this.ready = true;
 
   }
 
+  /* For one member, we group all his m_a_p inside the elements array.It's like a distinct member request */
   sortElements() {
     this.sorted_workers = [];
     let add: boolean;
@@ -87,15 +102,45 @@ export class VolumeProgressComponent implements OnInit {
       }
     }
 
+    //NOW I INSERT ALL M_A_P
     for (let s of this.sorted_workers){
       let sorted_map = [];
+      let total_target = 0;
       for (let m of this.m_a_p){
         if (s.member_id == m.member_id){
           sorted_map.push(m);
+          total_target += m.target_quantity;
         }
       }
-      let element = {member: s, m_a_ps: sorted_map}
+      let element = {member: s, m_a_ps: sorted_map, total_target: total_target};
       this.elements.push(element);
     }
   }
+
+  calendarRange() {
+    let start = new Date(this.project.project_start);
+    let end = new Date (this.project.project_end);
+    let intervalDay = this.dateInterval(start, end);
+    //let intervalMonth = Math.ceil(intervalDay/31);
+
+    // We get label of all months
+    var locale = "en-us";
+    let date = start;
+    let month:string;
+    for (let i = start.getMonth(); i<=end.getMonth(); i++){
+      month = date.toLocaleDateString(locale, { month: "long"});
+      this.months_label.push(month);
+      date.setMonth(date.getMonth()+1);
+    }
+
+
+
+  }
+
+  /* Give the number of days between 2 dates */
+  dateInterval (d1,d2){
+    var nb = d2.getTime() - d1.getTime();
+    return Math.ceil(nb/(1000*60*60*24));
+  }
+
 }
