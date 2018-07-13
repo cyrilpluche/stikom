1e20ed383cd7a3dde334e02eee9632cdbe536746
michelexe <enzo.fabre@orange.fr>
2018-07-13 13:21:37 +0200

    deploiement v1
    Signed-off-by: michelexe <enzo.fabre@orange.fr>

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, 'dist')));
+app.use('./dist');
 app.use('/static', express.static(__dirname + '/public'));
 
 const unlessPath = [
