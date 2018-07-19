34eb9141179dd267a6e6da41b48bd7441141561b
michelexe <enzo.fabre@orange.fr>
2018-07-19 10:45:19 +0200

    modification member unit project
    Signed-off-by: michelexe <enzo.fabre@orange.fr>

--- models/project_model.js
+++ models/project_model.js
@@ -32,10 +32,10 @@ let project = {
         })
     },
 
-    insertMemberUnitProject (unit_id, project_id, member_id) {
-        return db.any('INSERT INTO public.member_unit_project(unit_id, project_id, member_id)\n' +
-            'VALUES (${unit}, ${project}, ${member}) returning unit_id, project_id, member_id;',
-            {unit: unit_id, project: project_id, member: member_id}) .then(function (data) {
+    insertMemberUnitProject (unit_id, project_id, member_id, job_id) {
+        return db.any('INSERT INTO public.member_unit_project(unit_id, project_id, member_id, job_id)\n' +
+            'VALUES (${unit}, ${project}, ${member}, ${job}) returning unit_id, project_id, member_id;',
+            {unit: unit_id, project: project_id, member: member_id, job: job_id}) .then(function (data) {
             if (data.length === 0) {
                 return false
             } else {
