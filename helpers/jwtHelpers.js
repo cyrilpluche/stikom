16b50adf81495e19cc4452407173026c1eebf010
michelexe <enzo.fabre@orange.fr>
2018-07-31 12:21:13 +0200

    mise en place jwt
    Signed-off-by: michelexe <enzo.fabre@orange.fr>

--- helpers/jwtHelpers.js
+++ helpers/jwtHelpers.js
@@ -5,7 +5,7 @@ module.exports = {
     // creer un token pour un utilisateur
     jwtSignMember: function(member) {
         // Durée du token
-        const ON_WEEK = 60 * 60 * 24 * 7
+        const ON_WEEK = 60 * 60 * 24 * 7;
         return jwt.sign(member, process.env.JWT_SECRET, {expiresIn: ON_WEEK})
     },
     jwtCheckToken: function (req) {
@@ -14,7 +14,8 @@ module.exports = {
     },
     jwtDecode (req, callback) {
         if (this.jwtCheckToken(req)) {
-            let token = req.headers.authorization.split(' ')[1] || req.headers['x-access-token'] || req.body.token || req.params.token ;
+            let token = req.headers.authorization.split(' ')[1]
+                || req.headers['x-access-token'] || req.body.token || req.params.token ;
             jwt.verify(token, process.env.JWT_SECRET, function (err, decoded) {
                 callback(err, decoded)
             })
--- routes/activity_router.js
+++ routes/activity_router.js
@@ -118,7 +118,7 @@ router.get('/all_from_all_job_from_sop/:sop',
 
                        Promise.all(promises).then(function () {
                            sop.jobs = jobs;
-                           res.json(sop)
+                           res.json({data: sop})
                        }).catch(function (e) {
                            throw e
                        });
